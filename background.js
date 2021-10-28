7ad32565f466a530c36be9863daddae73a5f48b0
maxolee23 <maxolee23@gmail.com>
2021-10-28 14:17:43 -0500

    mvp

---
+++ background.js
@@ -0,0 +1,13 @@
+
+
+const contents = document.getElementById("related");
+const comments = document.getElementById("comments");
+
+
+comments.parentNode.removeChild(comments);
+related.parentNode.removeChild(related)
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,7 @@
+const contents = document.getElementById("related");
+const comments = document.getElementById("comments");
+
+
+contents.remove()
+comments.remove()
+
